c534bcd874454e6ea80fba1dace8f78589a2391a
thisisdigamber <thisisdigamber@gmail.com>
2025-02-11 21:24:37 +0530

    all basic files

---
+++ javascript/basic14/app.js
@@ -0,0 +1,60 @@
+// console.log("hello javascript");
+// let a = 5;
+// let b = 6 ;
+
+// console.log("a = " , 5);
+// console.log("b = " , 6);
+
+// console.log("sum of a + b is " , a+b);
+
+console.log("price of Burger");
+
+let price1 = 250;
+let price2 = 200;
+let price3 = 150;
+let price4 = 50;
+
+let size = "xl";
+
+
+if(size==="xl"){
+
+    console.log(`the price is ${price1}`);
+}
+else if(size=== "l"){
+
+    console.log(`the price is ${price2}`);
+}
+
+else if(size=== "m"){
+
+    console.log(`the price is ${price3}`);
+}
+else{
+
+    console.log(`the price is ${price4}`);
+}
+
+
+let marks = 95;
+
+if (marks>=33){
+
+    console.log("pass");
+
+    if (marks>=80){
+        
+        console.log("outstanding");
+
+    }
+
+    else{
+
+        console.log("A");
+    }
+}
+
+else{
+
+    console.log("better luck next time");
+}
